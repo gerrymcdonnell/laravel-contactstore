03ee0d29bbeb92f1f064e59a0920fa70dff8716c
Gerry Mc Donnell <irishbloke@gmail.com>
2018-02-17 14:21:10 +0000

    created vuew component

--- resources/assets/js/components/Contacts.vue
+++ resources/assets/js/components/Contacts.vue
@@ -1,13 +1,49 @@
 <!-- vue component -->
 <template>
-    <h1>contact vue component</h1>
+    <div>
+        <h1>contact vue component</h1>
+
+        <form action="#" @submit.prevent="edit ? updateContact(contact.id) : createContact()">
+
+            <div class="form-group">
+                <label>Name</label>
+                <input v-model="contact.name" type="text" name="name" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label>email</label>
+                <input v-model="contact.email" type="text" name="email" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label>phone</label>
+                <input v-model="contact.phone" type="text" name="phone" class="form-control">
+            </div>
+
+
+            <div class="form-group">
+                <button v-show="!edit" type="submit" class="btn btn-primary">New Contact </button>
+                <button v-show="edit" type="submit" class="btn btn-primary">Update Contact </button>
+            </div>
+
+        </form>
+    </div>
 </template>
 
 <script>
     export default {
 
         data(){
-            return{}
+            return{
+                edit:false,
+                list:[],
+                contact:{
+                    id:'',
+                    name:'',
+                    email:'',
+                    phone:''
+                }
+            }
         },
         mounted() {
             console.log('Contacts Component mounted.')
