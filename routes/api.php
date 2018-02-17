175e52a23fa772d2b672f57b263b284773b7b2ee
Gerry Mc Donnell <irishbloke@gmail.com>
2018-02-17 13:34:48 +0000

    setup api routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use App\Contact;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +13,49 @@ use Illuminate\Http\Request;
 |
 */
 
+//adding api for backend
+Route::group(['middleware'=>'api'],function(){
+
+    Route::get('contacts',function(){
+            return Contact::latest()->orderBy('created_at','desc')->get();
+    });
+
+    //single contact
+    Route::get('contact/{id}',function($id){
+        return Contact::findOrFail($id);
+    });
+
+    //add
+    Route::post('contact/store',function(Request $request){
+        return Contact::create(
+            [
+                'name'=>$request->input(['name']),
+                'email'=>$request->input(['email']),
+                'phone'=>$request->input(['phone'])
+            ]);
+    });
+
+    //update
+    Route::patch('contact/{id}',function(Request $request,$id){
+        Contact::findOrFail($id)->update(
+            [
+            'name'=>$request->input(['name']),
+            'email'=>$request->input(['email']),
+            'phone'=>$request->input(['phone'])
+            ]);
+    });
+
+    Route::delete('contact/{id}',function($id){
+        return Contact::destroy($id);
+    });
+
+
+
+});
+
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
